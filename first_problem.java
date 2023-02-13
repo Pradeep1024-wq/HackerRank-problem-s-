43bc033492b7d51acd4354ca11601c901415bd3b
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2023-02-13 11:09:55 +0530

    Create first_problem.java

---
+++ first_problem.java
@@ -0,0 +1,8 @@
+public class first_problem {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World.");
+         System.out.println("Hello, Java.");
+    }
+}
